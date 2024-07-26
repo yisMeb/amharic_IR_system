ad2ec6f87c5786e111d54ac4818131430546a3e2
yisak mebrate <yisakmebratedesalegn@gmail.com>
2024-07-26 15:02:12 +0300

    translation

--- stemming/main.py
+++ stemming/main.py
@@ -1,6 +1,20 @@
-import sys
-import io
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+import dict
 
+def amharic_to_english_converter(amharic_text):
+    translated_text = ""
+    for char in amharic_text:
+        if char in dict.amharic_to_english:
+            translated_text += dict.amharic_to_english[char]
+        else:
+            translated_text += char
+    return translated_text
 
+def main():
+    while True:
+        user_input = input("Enter Amharic text: ")
+        translated_text = amharic_to_english_converter(user_input)
+        print("Translated text:", translated_text)
+    
 
+if __name__ == "__main__":
+    main()
