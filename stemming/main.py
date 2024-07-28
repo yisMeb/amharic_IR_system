efed4b81727332c7e5d27dc2caf3d59882d3d154
yisak mebrate <yisakmebratedesalegn@gmail.com>
2024-07-28 13:18:42 +0300

    rule based

--- stemming/main.py
+++ stemming/main.py
@@ -1,15 +1,22 @@
 import tokenization
 import stemming
+import dict
 
+def replace_with_rule(token, rule):
+    for key, value in rule.items():
+        token = token.replace(key, value)
+    return token
 
 def main():
     user_input = input("Enter Amharic text: ").strip()
     tokens = tokenization.tokenize(user_input)
     print("Token: ",tokens)
     stemmed_tokens = [stemming.stem(token) for token in tokens]
-    print("Stemmed text:", ' '.join(stemmed_tokens))
+    final_tokens = [replace_with_rule(token, dict.Rule) for token in stemmed_tokens]
+    print("Final text:", ' '.join(final_tokens))
    
     
 if __name__ == "__main__":
     main()
+
 
